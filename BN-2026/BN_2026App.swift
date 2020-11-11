b312bc853f9ae0ca5d8b12248a7c9144d5ff319f
Patryk Strzemiecki <strzempa@wp.pl>
2020-11-11 11:01:53 +0100

    Initial Commit

---
+++ BN-2026/BN_2026App.swift
@@ -0,0 +1,17 @@
+//
+//  BN_2026App.swift
+//  BN-2026
+//
+//  Created by Patryk Strzemiecki on 11/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct BN_2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
